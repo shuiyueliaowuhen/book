f4ae82b0f5fed338aed006b363f5564d9ee052fe
songfei <784816716@qq.com>
2021-01-27 16:53:47 +0800

    列表

--- app/src/main/java/com/sf/bookapplaction/BookListActivity.java
+++ app/src/main/java/com/sf/bookapplaction/BookListActivity.java
@@ -2,6 +2,8 @@ package com.sf.bookapplaction;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class BookListActivity extends AppCompatActivity {
@@ -10,5 +12,10 @@ public class BookListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+    }
+
+    public static void start(Context context) {
+        Intent intent = new Intent(context, BookListActivity.class);
+        context.startActivity(intent);
     }
 }
